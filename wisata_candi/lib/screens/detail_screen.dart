b4ceb2da0639d24c8b1e3fe39a5ec0c1b48a591b
PiolaV <160827591+PiolaV@users.noreply.github.com>
2024-11-07 20:15:45 +0700

    1blmselesai

---
+++ wisata_candi/lib/screens/detail_screen.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class DetailScreen extends StatelessWidget {
+  final Candi candi;
+
+   const DetailScreen({super.key, required this.candi});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8),
+            child: ClipRRect
+            (borderRadius: BorderRadius.circular(20),
+            child: Image.asset(
+              candi.imageAsset,
+              width: double.infinity,
+              height: 300,
+              fit: BoxFit.cover,
+            ),
+            ),
+            ),
+
+
+        ],
+      ),
+    );
+  }
+
+}
