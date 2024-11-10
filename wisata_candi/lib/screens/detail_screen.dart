a862848b5908ed21c24444220936c3baaa7b5b67
PiolaV <160827591+PiolaV@users.noreply.github.com>
2024-11-10 20:29:14 +0700

    2

--- wisata_candi/lib/screens/detail_screen.dart
+++ wisata_candi/lib/screens/detail_screen.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
+//import 'package:candi_1/models/candi.dart';
 
 class DetailScreen extends StatelessWidget {
   final Candi candi;
 
-   const DetailScreen({super.key, required this.candi});
+  const DetailScreen({super.key, required this.candi});
   @override
   Widget build(BuildContext context) {
     return Scaffold(
