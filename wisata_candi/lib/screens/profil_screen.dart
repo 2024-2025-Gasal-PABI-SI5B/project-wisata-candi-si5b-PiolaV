813b211612da3ea3c69201684fcf269351d82c74
PiolaV <160827591+PiolaV@users.noreply.github.com>
2024-11-11 22:46:55 +0700

    p3Selesai

--- wisata_candi/lib/screens/profil_screen.dart
+++ wisata_candi/lib/screens/profil_screen.dart
@@ -10,9 +10,23 @@ class ProfilScreen extends StatefulWidget{
 class _ProfilScreenState extends State<ProfilScreen>{
   // todo: 1 dekrasikan variabel yang dibutuhkan 
   bool isSigneIn = false;
-  String fulName = '';
-  String userName = '';
+  String fullName = 'viola';
+  String userName = 'pio';
   int favoriteCandiCount = 0;
+
+  // TODO: 5 implementasi fungsi Sign in
+  void signIn(){
+    setState(() {
+      isSigneIn = !isSigneIn;
+
+    });
+  }
+  // TODO : 6 implementasi fungsi SignOut
+  void signOut(){
+    setState(() {
+      isSigneIn = !isSigneIn;
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,26 +42,139 @@ class _ProfilScreenState extends State<ProfilScreen>{
             child: Column(
               children: [
                 // TODO : 2 bagian profil hearder (ISINYA GAMBAR POFIL)
-                Stack(
-                  children: [
-                    Container(
-                      decoration: BoxDecoration(
-                        border: Border.all(
-                          color: Colors.deepPurple,
-                          width: 2,
+                Align(
+                  alignment: Alignment.topCenter,
+                  child: Padding(
+                    padding: const EdgeInsets.only(top: 150),
+                    child: Stack(
+                      alignment: Alignment.bottomRight,
+                      children: [
+                        Container(
+                          decoration: BoxDecoration(
+                            border: Border.all(
+                              color: Colors.deepPurple,
+                              width: 2,
+                            ),
+                            shape: BoxShape.circle
+                          ),
+                          child: const CircleAvatar(
+                            radius: 50,
+                            backgroundImage: AssetImage('images/placeholder_image.png'),
+                          ),
+                    
                         ),
-                        shape: BoxShape.circle
-                      ),
-                      child: CircleAvatar(
-                        radius: 50,
-                        backgroundImage: AssetImage('images/placeholder_image.png'),
-                      ),
+                        if(isSigneIn)
+                          IconButton(onPressed: (){}, 
+                          icon: Icon(
+                            Icons.camera_alt,
+                            color: Colors.deepPurple[50]))
+                    
+                      ],
+                    ),
+                  ),
+                ),
+                // TODO : 3 BUAT BAGIAN PROFIL INFO (ISINYA INFO PROFIL)
+                // baris 1 profil info 
+                const SizedBox(height: 20,),
+                Divider(color: Colors.deepPurple[100],),
+                const SizedBox(
+                  height: 4,
+                ),
+                Row(
+                  children: [
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width/3,
+                      child: const Row(
+                        children: [
+                          Icon(Icons.lock, color: Colors.amber,),
+                          const SizedBox(width: 8,),
+                          Text(
+                            'Pengguna', 
+                            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                            ),
 
+                        ],
+                      ),
+                    ),
+                    Expanded(child: Text(
+                      ': $userName',
+                    style: TextStyle(fontSize: 18),
+                    ),
                     )
                   ],
-                )
-                // TODO : 3 BUAT BAGIAN PROFIL INFO (ISINYA INFO PROFIL)
+                ),
+                // barris 2 nama 
+                const SizedBox(height: 20,),
+                Divider(color: Colors.deepPurple[100],),
+                const SizedBox(
+                  height: 4,
+                ),
+                Row(
+                  children: [
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width/3,
+                      child: const Row(
+                        children: [
+                          Icon(Icons.person, color: Colors.blue,),
+                          const SizedBox(width: 8,),
+                          Text(
+                            'Nama', 
+                            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                            ),
+
+                        ],
+                      ),
+                    ),
+                    Expanded(child: Text(
+                      ': $fullName',
+                    style: TextStyle(fontSize: 18),
+                      )),
+                      if(isSigneIn) const Icon(Icons.edit),
+                    
+
+                  ],
+                ),
+                // 3 favorit 
+                const SizedBox(height: 4,),
+                Divider(color: Colors.deepPurple[100],),
+                const SizedBox(
+                  height: 4,
+                ),
+                Row(
+                  children: [
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width/3,
+                      child: const Row(
+                        children: [
+                          Icon(Icons.favorite, color: Colors.red,),
+                          const SizedBox(width: 8,),
+                          Text(
+                            'Favorit', 
+                            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                            ),
+
+                        ],
+                      ),
+                    ),
+                    Expanded(child: Text(
+                      ': $favoriteCandiCount',
+                    style: TextStyle(fontSize: 18),
+                    ),
+                    )
+                  ],
+                ),
+
+
                 // TODO : 4 buat bagian profil action (isinya tombol signin/ signput)
+                const SizedBox(height: 4,),
+                Divider(color: Colors.deepPurple[100],),
+                const SizedBox(
+                  height: 20,
+                ),
+                isSigneIn 
+                ? TextButton(onPressed: signOut, child: Text('Sign Out'))
+                  : TextButton(onPressed: signIn, child: const Text('Sign In'))
+
               ],
             ),
           )
